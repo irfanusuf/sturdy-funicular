b306430511ca0fcc596648cb3cb7a0bab95023ec
irfan_usuf <irfanusuf33@gmail.com>
2025-05-12 13:44:45 +0530

    fixes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,93 @@
 const { User } = require("../models/userModel");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const registerController = async (req, res) => {
-  // const username = req.body
-  // const email = req.body
-  // const password = req.body
+  try {
+    // const username = req.body
+    // const email = req.body
+    // const password = req.body
 
-  // destructuring
-  const { username, email, password } = req.body;
+    // destructuring
+    const { username, email, password } = req.body;
 
-  //  server side validation
+    //  server side validation
 
-  if (username === "" || email === "" || password === "") {
-    return res
-      .status(400)
-      .json({
+    if (username === "" || email === "" || password === "") {
+      return res.status(400).json({
         message: "All the feilds with * are required !",
         success: false,
       });
-  }
+    }
 
-  const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-  if (existingUser) {
-    return res
-      .status(400)
-      .json({
+    if (existingUser) {
+      return res.status(400).json({
         message: "User Already Exists , kindly Go to login Page",
         success: false,
       });
-  }
+    }
 
-   const encryptPassWord =  await bcrypt.hash(password , 10)
-
-
-
-   const newUser = await User.create({ username, email, password : encryptPassWord });
-  
+    const encryptPassWord = await bcrypt.hash(password, 12); // syntatic sugar 
 
 
-  if (newUser) {
-    return res
-      .status(201)
-      .json({ message: "User Created succesfully!", success: true });
-  } else {
-    return res
-      .status(500)
-      .json({ message: "Something Went Wrong ", success: false });
+    const newUser = await User.create({
+      username,
+      email,
+      password: encryptPassWord,
+    });
+
+    if (newUser) {
+      return res
+        .status(201)
+        .json({ message: "User Created succesfully!", success: true });
+    } else {
+      return res.status(500).json({
+        message: "Something Went Wrong!",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error!" });
   }
 };
 
-const loginController = (req, res) => {};
+
+
+
+
+
+
+
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email === "" || password === "") {
+      return res
+        .status(400)
+        .json({ message: "Email and password both are required!" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(400).json({ message: "User not Found!" });
+    }
+
+    const verifyPass = await bcrypt.compare(password, existingUser.password);
+
+    if (verifyPass) {
+      return res.status(200).json({ message: "Login in succesfull!" });
+    } else {
+      return res.status(400).json({ message: "Password incorrect!" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server Error!" });
+  }
+};
 
 module.exports = {
   registerController,
