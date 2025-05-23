d17e5e27797ee9826426fd2a65f9e9697e84d76d
irfan_usuf <irfanusuf33@gmail.com>
2025-05-23 12:52:46 +0530

    verifyToken

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,35 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import loadingGIf from "../../assets/loading.gif"
+import loadingGIf from "../../assets/loading.gif";
+import axios from "axios";
 
 const Dashboard = () => {
   // simulate
 
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate()
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("Authorization Token");
 
-    if (token === "" || token === undefined || token === null) {   
+    if (token === "" || token === undefined || token === null) {
+      toast.error("Unauthorised!"); // notifications
 
-        toast.error("Unauthorised!")
-
-        setTimeout(() => {
-            navigate("/login")
-        }, 3000);
-     
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
     } else {
-      setLoading(false);
+      // IIF
+      (async () => {
+        try {
+          const url = `http://localhost:4000/verify/token?token=${token}`;
+
+          setTimeout(async() => {
+
+          const res = await axios.get(url); //backend  api call  for verification
+
+          if (res.status === 200) {
+            setLoading(false);
+          } else {
+            setLoading(true);
+          }
+            
+          }, 4000);
+
+
+        } catch (error) {
+          toast.error("Server Error ! try again after sometime");
+          setTimeout(() => {
+            navigate("/");
+          }, 10000);
+
+          console.error(error);
+        }
+      })();
     }
-  }, []);
+  }, [navigate]);
 
   return (
     <div className="container mt-5">
       {loading ? (
         <div className="Loading_Animation">
-          <img src={loadingGIf} alt="Loading....."/>
+          <img src={loadingGIf} alt="Loading....." />
         </div>
       ) : (
         <div>
