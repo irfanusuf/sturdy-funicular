9368fa9d793f402ec33d211f1b54de12c2b41caa
Irfan Yousuf <irfanusuf33@gmail.com>
2025-07-28 13:49:58 +0530

    deploy

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export const  axiosInstance  = axios.create({
-        baseURL : "http://localhost:4000",
+        // baseURL : "http://localhost:4000",
+        baseURL : "https://ok-6ys8.onrender.com",
         withCredentials : true
-        // baseURL : "https://www.something.com"
+    
     })
